d9da0469e0f92fea5c8fc2dbd87c1dd5c5b7ba93
Fariza Kubanova <farizatkubanova@gmail.com>
2022-08-05 18:07:54 +0100

    function changeColor

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 
-const div = document.querySelector('#message');
-const message = document.createElement('div');
+const message = document.querySelector('#message');
+const div = document.createElement('div');
 
 let cross=`<i class="fa-solid fa-xmark"></i>`;
+let circle=`<i class="fa-solid fa-o"></i>`
 let cells=document.querySelectorAll('#cell');
 for(let i=0;i<cells.length;i++){
     cells[i].addEventListener('click', click)
@@ -21,16 +22,24 @@ if(this.innerHTML==''){
 else {
     alert('ячейка занята')
 }
-if (this.innerHTML == `<i class="fa-solid fa-xmark"></i>`) {
+
+cross = cross == `<i class="fa-solid fa-xmark"></i>` ? `<i class="fa-solid fa-o"></i>` : `<i class="fa-solid fa-xmark"></i>`;
+changeColor();
+result();
+}
+
+
+function changeColor(){
+    if (this.innerHTML == cross) {
     document.querySelector('#circle').style.backgroundColor = 'pink';
     document.querySelector('#cross').style.backgroundColor = 'white';
-} else {
+} 
+if (this.innerHTML == circle) {
     document.querySelector('#circle').style.backgroundColor = 'white';
     document.querySelector('#cross').style.backgroundColor = 'yellow';
 }
-cross = cross == `<i class="fa-solid fa-xmark"></i>` ? `<i class="fa-solid fa-o"></i>` : `<i class="fa-solid fa-xmark"></i>`;
-result();
 }
+
 
 function result(){
     
@@ -50,18 +59,18 @@ let array=[
 ]
 
 for(let i=0;i<array.length;i++){
-    if (cells[array[i][0]].innerHTML===`<i class="fa-solid fa-xmark"></i>` && cells[array[i][1]].innerHTML===`<i class="fa-solid fa-xmark"></i>` && cells[array[i][2]].innerHTML===`<i class="fa-solid fa-xmark"></i>` && cells[array[i][3]].innerHTML===`<i class="fa-solid fa-xmark"></i>` && cells[array[i][4]].innerHTML===`<i class="fa-solid fa-xmark"></i>`)
+    if (cells[array[i][0]].innerHTML===cross && cells[array[i][1]].innerHTML===cross && cells[array[i][2]].innerHTML===cross && cells[array[i][3]].innerHTML===cross && cells[array[i][4]].innerHTML===cross)
         {
-        div.appendChild(message);
-        message.innerHTML = ` крестики победили`;
+        message.appendChild(div);
+        div.innerHTML = ` крестики победили`;
         document.querySelector('#message').style.zIndex = "1";
     }
     
-    else if (cells[array[i][0]].innerHTML==`<i class="fa-solid fa-o"></i>` && cells[array[i][1]].innerHTML==`<i class="fa-solid fa-o"></i>` && cells[array[i][2]].innerHTML==`<i class="fa-solid fa-o"></i>` && cells[array[i][3]].innerHTML==`<i class="fa-solid fa-o"></i>` && cells[array[i][4]].innerHTML==`<i class="fa-solid fa-o"></i>`)
+    else if (cells[array[i][0]].innerHTML===circle && cells[array[i][1]].innerHTML===circle && cells[array[i][2]].innerHTML===circle && cells[array[i][3]].innerHTML===circle && cells[array[i][4]].innerHTML===circle)
         {
             
-        div.appendChild(message);
-                message.innerHTML = `нолики победили`;
+        message.appendChild(div);
+                div.innerHTML = `нолики победили`;
                 document.querySelector('#message').style.zIndex = "1";
     }
 
